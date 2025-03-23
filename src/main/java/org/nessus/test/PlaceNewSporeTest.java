46bb950a3430e64ad046b54c922e95abeb810c22
Tancsics Gergely <tancsicsgergely1@gmail.com>
2025-03-23 10:08:51 +0100

    Spóraköpés/termelés tesztesetek

---
+++ src/main/java/org/nessus/test/PlaceNewSporeTest.java
@@ -0,0 +1,40 @@
+package org.nessus.test;
+
+import org.nessus.Skeleton;
+import org.nessus.model.Shroom;
+import org.nessus.model.ShroomBody;
+import org.nessus.model.Spore;
+import org.nessus.model.Tecton;
+
+public class PlaceNewSporeTest extends Test {
+    Shroom shroom;
+    ShroomBody body;
+    Tecton t1;
+    Spore spore;
+
+    public PlaceNewSporeTest(String name) {
+        super(name);
+    }
+
+    @Override
+    public void Init() {
+        shroom = new Shroom();
+        t1 = new Tecton();
+        body = new ShroomBody(shroom, t1);
+
+        Skeleton.AddObject(shroom, "shroom");
+        Skeleton.AddObject(body, "body");
+        Skeleton.AddObject(t1, "t1");
+        Skeleton.AddObject(shroom.GetActionPointCatalog(), "actCatalog");
+
+        shroom.SetShroomBody(body);
+        body.SetTecton(t1);
+        body.SetRemainingThrows(5);
+        body.SetSporeMaterials(5);
+    }
+
+    @Override
+    public void Run() {
+        shroom.ThrowSpore(body, t1);
+    }
+}
