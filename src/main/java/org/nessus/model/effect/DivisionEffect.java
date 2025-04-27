567e1ae4eaf454007c1e12a435a54fd14b35acbf
Tancsics Gergely <tancsicsgergely1@gmail.com>
2025-04-27 17:38:49 +0200

    DivisionEffect documented

--- src/main/java/org/nessus/model/effect/DivisionEffect.java
+++ src/main/java/org/nessus/model/effect/DivisionEffect.java
@@ -2,11 +2,27 @@ package org.nessus.model.effect;
 
 import org.nessus.model.bug.Bug;
 
+/**
+ * Az osztály a "felosztás" hatását reprezentálja.
+ * A "felosztás" hatására a rovarok felosztják magukat.
+ * A hatás három alkalommal alkalmazható.
+ * A {@link BugEffect} osztályból származik le.
+ * @see org.nessus.model.bug.Bug
+ * @see org.nessus.model.effect.BugEffect
+ */
 public class DivisionEffect extends BugEffect {
+    /**
+     * Az effekt inicializálása, a hátralévő használatok számának beállítása 0-ra.
+     */
     public DivisionEffect() {
         remainingUses = 0;
     }
 
+    /**
+     * A hatás alkalmazása a rovarokra.
+     * @param bug - A rovar, akire a hatást alkalmazzuk.
+     * @return void
+     */
     public void ApplyOn(Bug bug) {
         bug.ClearEffect(this);
         bug.Split();
