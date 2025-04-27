1779228f831cf72a6eafc335bd74099c0483c676
Milan <hmilan56@gmail.com>
2025-04-27 20:36:41 +0200

    fix linux shell script

--- run_tests.sh
+++ run_tests.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
-echo
-(
-echo runall
-echo exit
-) | java -jar target/fungorium-prototype-1.0.jar
+# Run the Java program with input "runall" and "exit"
+echo -e "runall\nexit" | java -jar target/fungorium-prototype-1.0.jar
 
 echo "Comparing outputs..."
-for i in {1..34}
+
+# Loop from 1 to 34
+for i in $(seq 1 34)
 do
-    if ! diff -q "src/main/resources/test/test$i/expected.txt" "src/main/resources/test/test$i/output.txt" > /dev/null
-    then
+    if ! diff "src/main/resources/test/test$i/expected.txt" "src/main/resources/test/test$i/output.txt" > /dev/null; then
         echo "Difference found in test $i!"
         diff "src/main/resources/test/test$i/expected.txt" "src/main/resources/test/test$i/output.txt"
         echo "---------------------------------------------"
