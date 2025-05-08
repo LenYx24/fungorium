aeb178eaae2d23cca8e62eb21970c33c2caf6888
Tancsics Gergely <tancsicsgergely1@gmail.com>
2025-05-08 10:52:52 +0200

    Preps

--- src/main/java/org/nessus/view/panels/SettingsPanel.java
+++ src/main/java/org/nessus/view/panels/SettingsPanel.java
@@ -1,5 +1,6 @@
 package org.nessus.view.panels;
 
+import org.nessus.model.bug.BugOwner;
 import org.nessus.view.BaseButton;
 import org.nessus.view.View;
 
@@ -17,9 +18,25 @@ public class SettingsPanel extends JPanel {
         JPanel panel = new JPanel();
         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
 
-        JLabel label = new JLabel("SETTINGS NOOOO LA POLICIJA",SwingConstants.CENTER);
+        JLabel label = new JLabel("JÁTÉK BEÁLLÍTÁSA",SwingConstants.CENTER);
         label.setAlignmentX(Component.CENTER_ALIGNMENT);
         label.setFont(new Font("Roboto", Font.BOLD, 40));
+
+        // 2x2 grid
+        JPanel griJPanel = new JPanel();
+        griJPanel.setLayout(new GridLayout(2, 2));
+
+        // 0,0
+        JLabel gombaszLabel = new JLabel("Gombászok");
+        gombaszLabel.setFont(new Font("Roboto", Font.BOLD, 20));
+        gombaszLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        griJPanel.add(gombaszLabel, 0, 0);
+
+        // 1,0
+        JLabel rovaraszLabel = new JLabel("Rovarászok");
+        rovaraszLabel.setFont(new Font("Roboto", Font.BOLD, 20));
+        rovaraszLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        griJPanel.add(rovaraszLabel, 0, 1);
 
         JButton newGameButton = new BaseButton("vissza");
         newGameButton.addActionListener(e -> {
@@ -29,6 +46,7 @@ public class SettingsPanel extends JPanel {
         });
 
         panel.add(label);
+        panel.add(griJPanel);
         panel.add(Box.createRigidArea(new Dimension(0, 30)));
         panel.add(newGameButton);
 
