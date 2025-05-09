7f699a8d88d0155a99ef9a0224c2f9cde3430d62
Tancsics Gergely <tancsicsgergely1@gmail.com>
2025-05-09 13:19:49 +0200

    Settings gui finished

--- src/main/java/org/nessus/view/panels/SettingsPanel.java
+++ src/main/java/org/nessus/view/panels/SettingsPanel.java
@@ -42,9 +42,87 @@ public class SettingsPanel extends JPanel {
         griJPanel.add(rovaraszLabel);
 
         JPanel settingsLeft = new JPanel();
+        settingsLeft.setLayout(new BoxLayout(settingsLeft, BoxLayout.Y_AXIS));
+
         JPanel settingsRight = new JPanel();
-        griJPanel.add(settingsLeft);
-        griJPanel.add(settingsRight);
+        settingsRight.setLayout(new BoxLayout(settingsRight, BoxLayout.Y_AXIS));
+
+        Color[] gombaszColors = {Color.RED, Color.GREEN, Color.BLUE};
+        for (int i = 0; i < 3; i++) {
+            final int playerNum = i + 1;
+            JCheckBox checkBox = new JCheckBox();
+            checkBox.setMargin(new Insets(0, 0, 0, 0));
+            
+            JTextField textField = new JTextField("Gombász" + playerNum);
+            textField.setPreferredSize(new Dimension(100, 25));
+            textField.setEnabled(false);
+            textField.setForeground(gombaszColors[i]);
+            
+            JPanel colorBox = new JPanel();
+            colorBox.setPreferredSize(new Dimension(20, 20));
+            colorBox.setBackground(gombaszColors[i]);
+            colorBox.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+            checkBox.addActionListener(e -> {
+                boolean isSelected = checkBox.isSelected();
+                textField.setEnabled(isSelected);
+                if (isSelected) {
+                    textField.setText("Gombász" + playerNum);
+                }
+            });
+
+            JPanel playerRow = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
+            playerRow.add(checkBox);
+            playerRow.add(textField);
+            playerRow.add(colorBox);
+            settingsLeft.add(playerRow);
+        }
+
+        Color[] rovaraszColors = {Color.WHITE, Color.GRAY, new Color(139, 69, 19)};
+        for (int i = 0; i < 3; i++) {
+            final int playerNum = i + 1;
+            JCheckBox checkBox = new JCheckBox();
+            checkBox.setMargin(new Insets(0, 0, 0, 0));
+            
+            JTextField textField = new JTextField("Rovarász" + playerNum);
+            textField.setPreferredSize(new Dimension(100, 25));
+            textField.setEnabled(false);
+            textField.setForeground(rovaraszColors[i]);
+            
+            JPanel colorBox = new JPanel();
+            colorBox.setPreferredSize(new Dimension(20, 20));
+            colorBox.setBackground(rovaraszColors[i]);
+            colorBox.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+            checkBox.addActionListener(e -> {
+                boolean isSelected = checkBox.isSelected();
+                textField.setEnabled(isSelected);
+                if (isSelected) {
+                    textField.setText("Rovarász" + playerNum);
+                }
+            });
+
+            JPanel playerRow = new JPanel(new FlowLayout(FlowLayout.LEFT, 5, 0));
+            playerRow.add(checkBox);
+            playerRow.add(textField);
+            playerRow.add(colorBox);
+            settingsRight.add(playerRow);
+        }
+
+        JPanel leftWrapper = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        leftWrapper.add(settingsLeft);
+        griJPanel.add(leftWrapper);
+
+        JPanel rightWrapper = new JPanel(new FlowLayout(FlowLayout.CENTER, 0, 0));
+        rightWrapper.add(settingsRight);
+        griJPanel.add(rightWrapper);
+
+        panel.add(label);
+        panel.add(griJPanel);
+        panel.add(Box.createRigidArea(new Dimension(0, 20)));
+
+        JPanel tectonPanel = new JPanel();
+        tectonPanel.setLayout(new GridLayout(1, 2));
 
         JLabel tectonNumber = new JLabel("Tektonok száma:");
         tectonNumber.setFont(new Font("Roboto", Font.BOLD, 20));
@@ -70,22 +148,22 @@ public class SettingsPanel extends JPanel {
         JPanel leftPanel = new JPanel();
         leftPanel.add(tectonNumber);
         leftPanel.add(intInput);
-        griJPanel.add(leftPanel);
 
         JButton actionButton = new BaseButton("Következő");
         JPanel rightPanel = new JPanel();
         rightPanel.add(actionButton);
-        griJPanel.add(rightPanel);
 
-        JButton newGameButton = new BaseButton("vissza");
+        JButton newGameButton = new BaseButton("Vissza");
         newGameButton.addActionListener(e -> {
             System.out.println("VISSZA!%");
             CardLayout cardLayout = (CardLayout)mainPanel.getLayout();
             cardLayout.show(mainPanel,"menu");
         });
 
-        panel.add(label);
-        panel.add(griJPanel);
+        tectonPanel.add(leftPanel);
+        tectonPanel.add(rightPanel);
+
+        panel.add(tectonPanel);
         panel.add(Box.createRigidArea(new Dimension(0, 30)));
         panel.add(newGameButton);
 
