8a7ef080334a6f0b2d77fe3e2696c725ba404bdb
Tancsics Gergely <tancsicsgergely1@gmail.com>
2025-05-18 21:20:09 +0200

    Added comment to BGMPlayer class

--- src/main/java/org/nessus/utility/BGMPlayer.java
+++ src/main/java/org/nessus/utility/BGMPlayer.java
@@ -4,30 +4,32 @@ import javax.sound.sampled.*;
 import java.io.File;
 import java.io.IOException;
 
-public class BGMPlayer
-{
-    private Clip clip;
+/**
+ * BGMP Player osztály, amely a háttérzene lejátszásáért felelős
+ */
+public class BGMPlayer {
+    private Clip clip; // A zene
 
-    public BGMPlayer(String path)
-    {
-        try
-        {
+    /**
+     * Konstruktor, paraméterként megadhatjuk neki a zenét
+     * @param path - A zene elérési útvonala
+     */
+    public BGMPlayer(String path) {
+        try {
             AudioInputStream audioStream = AudioSystem.getAudioInputStream(new File(path));
             clip = AudioSystem.getClip();
             clip.open(audioStream);
-
-        }
-        catch (UnsupportedAudioFileException | IOException | LineUnavailableException e)
-        {
+        } catch (UnsupportedAudioFileException | IOException | LineUnavailableException e) {
             e.printStackTrace();
         }
     }
 
-    public void playLoop()
-    {
-        if (clip != null)
-        {
-            clip.loop(Clip.LOOP_CONTINUOUSLY); // Loops the music
+    /**
+     * Zene lejátszási ciklus, folyamatosan játssza a zenét
+     */
+    public void playLoop() {
+        if (clip != null) {
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
             clip.start();
         }
     }
